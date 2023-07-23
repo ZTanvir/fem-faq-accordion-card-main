2fe41853f317eedc0c8830d0e9564f48e7debe1d
ztanvir <zahirulopel@gmail.com>
2023-07-23 17:12:57 +0600

    Add bold style to font and rotate the arrow image

--- script/script.js
+++ script/script.js
@@ -5,12 +5,19 @@ let FAQ_QUESTIONS_ARRAY = [...FAQ_QUESTIONS];
 FAQ_QUESTIONS_ARRAY.forEach((faqQstn) => {
   faqQstn.addEventListener("click", (e) => {
     const ELEMENT = e.target.parentElement.nextElementSibling;
+    const QUESTION = e.target.parentElement.firstElementChild;
+    const ARROWIMAGE = e.target.parentElement.lastElementChild;
+
+    console.log(ARROWIMAGE);
+
     console.log(ELEMENT);
 
     // Check if the user click on the faq question or faq arrow key
     if (ELEMENT.classList.contains("faq-answer")) {
       ELEMENT.classList.toggle("hide-qa-answer");
-      ELEMENT.style.opacity = "0.7";
+      // add styles to the faq question
+      QUESTION.classList.toggle("bold-faq-question");
+      ARROWIMAGE.classList.toggle("rotate-question-arrow");
     }
   });
 });
